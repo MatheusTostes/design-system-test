9362ad1f0fe12845a31560fe7bb86fe523588051
Tostes <matheus_tostes@hotmail.com>
2024-12-02 02:48:26 -0300

    chore(vercel): deploy adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -14,21 +14,25 @@ export default defineConfig({
     }),
   ],
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/components/index.ts"),
-      name: "YourLibrary",
-      formats: ["es", "umd"],
-      fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+    ...(process.env.BUILD_LIB
+      ? {
+          lib: {
+            entry: path.resolve(__dirname, "src/components/index.ts"),
+            name: "YourLibrary",
+            formats: ["es", "umd"],
+            fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
+          },
+          rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+              globals: {
+                react: "React",
+                "react-dom": "ReactDOM",
+              },
+            },
+          },
+        }
+      : undefined),
   },
   resolve: {
     alias: {
